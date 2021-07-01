54c90c15534c0cec46315c175e52d4f1945060cf
João Neto <l.joaont@gmail.com>
2021-07-01 10:16:38 -0300

    Domain model, database seed, pom version downgrade

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entitites/Seller.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dsvendas.entitites;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "tb_sellers")
+@Entity
+public class Seller {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+
+	public Seller(){}
+
+	public Long getId () {
+		return id;
+	}
+
+	public void setId (Long id) {
+		this.id = id;
+	}
+
+	public String getName () {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public List<Sale> getSales () {
+		return sales;
+	}
+
+	public void setSales (List<Sale> sales) {
+		this.sales = sales;
+	}
+}
